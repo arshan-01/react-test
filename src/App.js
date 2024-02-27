1b31bc5ed5d24436ca1b50704af5d378686fcce2
Arshan Nawaz <arshannawaz.one@gmail.com>
2024-02-28 01:31:44 +0500

    eslint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import Routing from "./Routing/Routing";
 
 const App = () => {
+  const apple = 2;
+
+  let str = "jkhkj";
+  console.log("hgkhsgk");
+
   return <Routing />;
 };
 
